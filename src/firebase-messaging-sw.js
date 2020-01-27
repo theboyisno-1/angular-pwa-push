4ca0b310c9ac84ba6d51d119bd15497670a32a62
Abdul Khan <Khan019950@gmail.com>
2020-01-27 17:57:48 +0530

    Update firebase-messaging-sw.js

--- src/firebase-messaging-sw.js
+++ src/firebase-messaging-sw.js
@@ -2,12 +2,12 @@ importScripts('https://www.gstatic.com/firebasejs/4.12.1/firebase-app.js');
 importScripts('https://www.gstatic.com/firebasejs/4.12.1/firebase-messaging.js');
 
 var config = {
-    apiKey: 'AIzaSyBkPUel0bn-Ful3kk1Nrkr79vmoSFOhlW8',
-  authDomain: 'pwa-test-d6861.firebaseapp.com',
+    apiKey: 'api-key',
+  authDomain: 'pwa-test.firebaseapp.com',
   databaseURL: 'https://pwa-test-d6861.firebaseio.com',
-  projectId: 'pwa-test-d6861',
-  storageBucket: 'pwa-test-d6861.appspot.com',
-  messagingSenderId: '8978710009561'
+  projectId: 'pwa-test',
+  storageBucket: 'pwa-test.appspot.com',
+  messagingSenderId: 'Id'
 };
 firebase.initializeApp(config);
 
